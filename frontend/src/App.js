f99b1f7924698110e92971619ea0001c2a1e2084
Unwana Essien <unwana.essien@outlook.com>
2019-01-12 15:20:53 +0100

    basic modifications to make phaser work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,60 @@
-import React, { Component } from 'react';
-import Phaser from 'phaser';
+import React, { Component } from "react";
+import Phaser from "phaser";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-  componentDidMount() {
-    var config = {
-      type: Phaser.AUTO,
-      width: 800,
-      height: 600,
-      parent: 'phaser-target',
-      physics: {
-          default: 'arcade',
-          arcade: {
-              debug: true
-          }
-      },
-      scene: {
-          preload: this.preload,
-          create: this.create,
-          update: this.update
-      },
-      player_speed: 150
-    };
-    var game = new Phaser.Game(config);
-    var player;
-    var cursors;
-
-    this.preload = () => {
-      this.load.image('block', 'assets/star.png');
+    constructor(props) {
+        super(props);
     }
-    this.create = () => {
-      cursors = this.input.keyboard.createCursorKeys();
-      player = this.physics.add.sprite(400, 300, 'block');
-    }
-    this.update = () => {
-      player.setVelocity(0);
-      if (cursors.left.isDown && !cursors.right.isDown){
-          player.setVelocityX(-config.player_speed);
-      }
-      if (cursors.right.isDown && !cursors.left.isDown) {
-          player.setVelocityX(config.player_speed);
-      }
-      if (cursors.up.isDown && !cursors.down.isDown){
-          player.setVelocityY(-config.player_speed);
-      }
-      if (cursors.down.isDown && !cursors.up.isDown) {
-          player.setVelocityY(config.player_speed);
-      }
-    }
-  }
 
-  render() {
-    return (
-      <section id="phaser-target"></section>
-    )
-  }
+    componentDidMount() {
+        var config = {
+            type: Phaser.AUTO,
+            width: 800,
+            height: 600,
+            parent: "phaser-target",
+            physics: {
+                default: "arcade",
+                arcade: {
+                    debug: true
+                }
+            },
+            scene: {
+                preload: preload,
+                create: create,
+                update: update
+            },
+            player_speed: 150
+        };
+        var game = new Phaser.Game(config);
+        var player;
+        var cursors;
+
+        function preload() {
+            this.load.setBaseURL("http://labs.phaser.io");
+            this.load.image("block", "assets/sprites/phaser3-logo.png");
+        };
+        function create() {
+            cursors = this.input.keyboard.createCursorKeys();
+            player = this.physics.add.sprite(400, 300, "block");
+        };
+        function update() {
+            player.setVelocity(0);
+            if (cursors.left.isDown && !cursors.right.isDown) {
+                player.setVelocityX(-config.player_speed);
+            }
+            if (cursors.right.isDown && !cursors.left.isDown) {
+                player.setVelocityX(config.player_speed);
+            }
+            if (cursors.up.isDown && !cursors.down.isDown) {
+                player.setVelocityY(-config.player_speed);
+            }
+            if (cursors.down.isDown && !cursors.up.isDown) {
+                player.setVelocityY(config.player_speed);
+            }
+        };
+    }
+
+    render() {
+        return <section id="phaser-target" />;
+    }
 }
